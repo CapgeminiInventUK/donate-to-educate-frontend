4b2a220cf8cb120fa1485b5167424792d410db33
Ryan Smith <ryan.smith1@bp.com>
2023-07-28 10:38:11 +0100

    feat: Move to React

---
+++ .eslintrc.cjs
@@ -0,0 +1,55 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'prettier/prettier': [
+      'warn',
+      {
+        endOfLine: 'auto',
+      },
+      {
+        usePrettierrc: true,
+      },
+    ],
+    'no-console': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/unbound-method': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/quotes': [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+    'jsx-quotes': [2, 'prefer-double'],
+    '@typescript-eslint/func-call-spacing': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/await-thenable': 'off',
+  },
+};
--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,31 @@
-/* eslint-disable no-console */
-import { JSX } from 'preact';
-import { useState } from 'preact/hooks';
-import preactLogo from './assets/preact.svg';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './app.css';
+import './App.css';
 
-export function App(): JSX.Element {
+function App(): JSX.Element {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://preactjs.com" target="_blank" rel="noreferrer">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
+      <h1>Vite + React</h1>
+      <div className="card">
         <button onClick={(): void => setCount((count) => count + 1)}>count is {count}</button>
         <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p class="read-the-docs">Click on the Vite and Preact logos to learn more</p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
+
+export default App;
