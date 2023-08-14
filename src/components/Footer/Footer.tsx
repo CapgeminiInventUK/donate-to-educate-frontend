bd9d3269274714cc52e3adf5b03054b5349fff83
Ryan Smith <ryan.smith1@bp.com>
2023-08-14 15:04:35 +0100

    feat: Create initial mobile footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,28 @@
 import { ReactElement, FC } from 'react';
 import styles from './Footer.module.scss';
 import LogoWhite from '../../assets/logo/LogoWhite';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Footer: FC = (): ReactElement => {
+  const navigate = useNavigate();
   return (
     <footer className={styles.container}>
       <LogoWhite className={styles.logo} />
+      <button onClick={(): void => navigate('/contact')} className={styles.button}>
+        Contact us
+      </button>
+      <div className={styles.links}>
+        <Link to={'/'} className={styles.link}>
+          Accessibility statement
+        </Link>
+        <Link to={'/'} className={styles.link}>
+          Privacy Policy
+        </Link>
+        <Link to={'/'} className={styles.link}>
+          Terms and conditions
+        </Link>
+      </div>
+      <p>Educate to Donate &copy;</p>
     </footer>
   );
 };
