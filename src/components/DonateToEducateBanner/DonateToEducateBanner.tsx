cff7cbc037d67ed3339e7ce98c8a450605b04485
Ryan Smith <ryan.smith1@bp.com>
2023-08-31 13:53:12 +0100

    feat: PR comments

--- src/components/DonateToEducateBanner/DonateToEducateBanner.tsx
+++ src/components/DonateToEducateBanner/DonateToEducateBanner.tsx
@@ -2,12 +2,9 @@ import { FC } from 'react';
 import styles from './DonateToEducateBanner.module.scss';
 import LogoWhite from '@/assets/logo/LogoWhite';
 
-const DonateToEducateBanner: FC = () => {
-  return (
-    <div className={styles.container}>
-      <LogoWhite className={styles.logo} />
-    </div>
-  );
-};
-
+const DonateToEducateBanner: FC = () => (
+  <div className={styles.container}>
+    <LogoWhite className={styles.logo} />
+  </div>
+);
 export default DonateToEducateBanner;
