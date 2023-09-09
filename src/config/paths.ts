e2584cd5356516697b3bcc3f809214f69fd9db70
Nicholas Mann <trekiman@gmail.com>
2023-09-09 23:32:07 +0100

    change dev url

--- src/config/paths.ts
+++ src/config/paths.ts
@@ -12,7 +12,7 @@ enum Paths {
   PTA = 'https://www.pta.co.uk/',
   FUNDED = 'https://www.funded.org.uk',
   INVENT = 'https://www.capgemini.com/about-us/who-we-are/our-brands/capgemini-invent/',
-  DEV_PREVIEW = '/dev-preview',
+  DEV_PREVIEW = '/6465762d70726576696577',
 }
 
 export default Paths;
