9ab1ce395e1c26771c95e106444c572e1ea83fdb
Ryan Smith <ryan.smith1@bp.com>
2023-11-13 22:21:37 +0000

    feat: add identity pool config

--- src/amplify.config.ts
+++ src/amplify.config.ts
@@ -6,6 +6,7 @@ const envSchema = z.object({
   VITE_COGNITO_USER_POOLS_ID: z.string().min(1),
   VITE_APPSYNC_API_KEY: z.string().min(1),
   VITE_ANALYTICS_APP_ID: z.string().min(1),
+  VITE_COGNITO_IDENTITY_POOLS_ID: z.string().min(1),
 });
 
 const env = envSchema.parse(import.meta.env);
@@ -24,6 +25,8 @@ export const amplifyConfig = {
   aws_cognito_region: env.VITE_AWS_REGION,
   aws_user_pools_id: env.VITE_COGNITO_USER_POOLS_ID,
   aws_user_pools_web_client_id: '68vvv67nra2567fl2m83so533k',
+  aws_cognito_identity_pool_id: env.VITE_COGNITO_IDENTITY_POOLS_ID,
+  aws_mandatory_sign_in: false,
 
   // Pinpoint
   aws_mobile_analytics_app_region: env.VITE_AWS_REGION,
