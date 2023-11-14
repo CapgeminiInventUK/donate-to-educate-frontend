075809f9a1098dc35545a073c5e0f7274550d64a
Ryan Smith <ryan.smith1@bp.com>
2023-11-14 20:55:25 +0000

    feat: auto track page view

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 import './index.scss';
-import { Amplify } from 'aws-amplify';
+import { Amplify, Analytics } from 'aws-amplify';
 import { amplifyConfig } from './amplify.config';
 
 Amplify.configure(amplifyConfig);
+Analytics.autoTrack('pageView', {
+  enable: true,
+  type: 'SPA',
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
