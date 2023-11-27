cddaf4ebfc993c75cf4966d7b41fe261e63cf378
Jake Readman <jakereadman@gmail.com>
2023-11-27 19:58:51 +0000

    chore: create form component function to utils

--- src/pages/Form/FormContainer.tsx
+++ src/pages/Form/FormContainer.tsx
@@ -1,40 +1,11 @@
-import {
-  CheckboxProps,
-  FormContainerProps,
-  RadioGroupProps,
-  TextAreaProps,
-  TextInputProps,
-} from '@/types/props';
-import { FC, ReactNode } from 'react';
+import { FormContainerProps } from '@/types/props';
+import { FC } from 'react';
 import styles from './Form.module.scss';
-import TextInput from '@/components/TextInput/TextInput';
-import RadioGroup from '@/components/RadioGroup/RadioGroup';
-import Checkbox from '@/components/Checkbox/Checkbox';
 import Button from '@/components/Button/Button';
-import { ComponentDataPropsType, ComponentType } from '@/types/data';
 import ExternalLink from '@/components/ExternalLink/ExternalLink';
-import TextArea from '@/components/TextArea/TextArea';
+import { createFormComponent } from '@/utils/components';
 
 const FormContainer: FC<FormContainerProps> = ({ formData, pageNumber, setPageNumber }) => {
-  const createFormComponent = (
-    componentType: ComponentType,
-    componentData: ComponentDataPropsType
-  ): ReactNode => {
-    switch (componentType) {
-      case ComponentType.TEXT:
-        return <TextInput {...(componentData as TextInputProps)} />;
-      case ComponentType.TEXTAREA:
-        return <TextArea {...(componentData as TextAreaProps)} />;
-      case ComponentType.RADIO:
-        return <RadioGroup {...(componentData as RadioGroupProps)} />;
-      case ComponentType.CHECKBOX:
-        return <Checkbox {...(componentData as CheckboxProps)} />;
-
-      default:
-        return <></>;
-    }
-  };
-
   const onButtonClick = (): void => {
     if (pageNumber < formData.length - 1) {
       setPageNumber(pageNumber + 1);
