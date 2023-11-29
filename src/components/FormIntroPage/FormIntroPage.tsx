8260dbd21dd453420e1941311e827c08599680f4
viki3612 <viktorianikolova95@gmail.com>
2023-11-29 14:59:41 +0000

    formintropage

--- src/components/FormIntroPage/FormIntroPage.tsx
+++ src/components/FormIntroPage/FormIntroPage.tsx
@@ -1,25 +1,41 @@
 import { FC } from 'react';
+import styles from './FormIntroPage.module.scss';
 
 interface Props {
   header: string;
   infoText: string;
   listItems: string[];
   secondaryHeading?: string;
-  secondaryInfoText: string;
-  secondaryListItems: string[];
+  secondaryInfoText?: string;
+  secondaryListItems?: string[];
 }
 
 const FormIntroPage: FC<Props> = ({
   header,
-  //   infoText,
-  //   listItems,
-  //   secondaryHeading,
-  //   secondaryInfoText,
-  //   secondaryListItems,
+  infoText,
+  listItems,
+  secondaryHeading,
+  secondaryInfoText,
+  secondaryListItems,
 }) => {
   return (
-    <div>
-      <h1>{header}</h1>
+    <div className={styles.container}>
+      <div className={styles.contentContainer}>
+        <div className={styles.card}>
+          <h2 className={styles.header}>{header} </h2>
+          <p className={styles.text}> {infoText}</p>
+          <ul className={styles.text}>
+            {listItems.map((item, id) => (
+              <li key={id}>{item}</li>
+            ))}
+          </ul>
+          <h3 className={styles.secondaryHeader}>{secondaryHeading}</h3>
+          <p className={styles.text}>{secondaryInfoText}</p>
+          <ul className={styles.text}>
+            {secondaryListItems?.map((item, id) => <li key={id}>{item}</li>)}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Form/Form.tsx
+++ src/pages/Form/Form.tsx
@@ -1,11 +1,40 @@
 import { FormProps } from '@/types/props';
 import { FC } from 'react';
+import FormIntroPage from '@/components/FormIntroPage/FormIntroPage';
 
 const Form: FC<FormProps> = ({ formData }) => {
   // eslint-disable-next-line no-console
   console.log(formData);
   // const [pageNumber, setPageNumber] = useState(0);
-  return <div>Form</div>;
+
+  const pageData = {
+    header: 'Join Donate to Educate',
+    infoText: 'To support us and help families and schools, you need to:',
+    listItems: [
+      'work at a charity or be a volunteer',
+      'have a work address or premises to store school products',
+      'provide us with your contact details',
+      'review and agree to our GDPR statements and policies',
+      'agree that we can share your information with your local authority or local council to confirm your identity',
+    ],
+    secondaryHeading: 'What to expect',
+    secondaryInfoText: `One you give us your details, we will:`,
+    secondaryListItems: [
+      `send you details to your local authority or local council`,
+      `your local authority or local council will confirm your identity`,
+      `you will get an email to confirm whether you can join`,
+    ],
+  };
+  return (
+    <FormIntroPage
+      header={pageData.header}
+      infoText={pageData.infoText}
+      listItems={pageData.listItems}
+      secondaryHeading={pageData.secondaryHeading}
+      secondaryInfoText={pageData.secondaryInfoText}
+      secondaryListItems={pageData.secondaryListItems}
+    />
+  );
 };
 
 export default Form;
