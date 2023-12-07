10b4eed0fe111cd4d4f0826b053ad29ebc53b2d0
Ryan Smith <ryan.smith1@bp.com>
2023-12-07 16:07:00 +0000

    feat: add url provider and session tracking

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,19 @@ import { configureAutoTrack } from 'aws-amplify/analytics';
 import { amplifyConfig } from './amplify.config';
 
 Amplify.configure(amplifyConfig);
-configureAutoTrack({ enable: true, type: 'pageView', options: { appType: 'singlePage' } });
+configureAutoTrack({
+  enable: true,
+  type: 'pageView',
+  options: {
+    appType: 'singlePage',
+    eventName: 'pageView',
+    urlProvider: () => window.location.origin + window.location.pathname,
+  },
+});
+configureAutoTrack({
+  enable: true,
+  type: 'session',
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
