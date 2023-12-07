edfac4569a4f83c1bfd3634cca915133feb63e56
Robin Moffatt <robinmoffatt@Robins-MacBook-Pro.local>
2023-12-07 16:56:11 +0000

    feat:(la_sign_up): use user inputs on local authority sign up

--- src/pages/AdminDashboard/LocalAuthoritySignUp.tsx
+++ src/pages/AdminDashboard/LocalAuthoritySignUp.tsx
@@ -1,5 +1,5 @@
 import TextInput from '@/components/TextInput/TextInput';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import styles from './LocalAuthoritySignUp.module.scss';
 import FormButton from '@/components/FormButton/FormButton';
 import TextArea from '@/components/TextArea/TextArea';
@@ -15,6 +15,17 @@ interface LocalAuthoritySignUpProps {
 }
 
 const LocalAuthoritySignUp: FC<LocalAuthoritySignUpProps> = ({ name, setStage }) => {
+  const [localState, setLocalState] = useState({
+    name: name,
+    firstName: '',
+    lastName: '',
+    jobTitle: '',
+    department: '',
+    email: '',
+    phone: '',
+    notes: '',
+  });
+
   const { refetch } = useQuery({
     queryKey: ['register'],
     enabled: false,
@@ -22,13 +33,14 @@ const LocalAuthoritySignUp: FC<LocalAuthoritySignUpProps> = ({ name, setStage })
       const result = await client.graphql<GraphQLQuery<RegisterLocalAuthorityMutation>>({
         query: registerLocalAuthority,
         variables: {
-          name,
-          firstName: '1',
-          lastName: '2',
-          jobTitle: '1',
-          department: '1',
-          email: '1',
-          phone: '1',
+          name: name,
+          firstName: localState.firstName,
+          lastName: localState.lastName,
+          jobTitle: localState.jobTitle,
+          department: localState.department,
+          email: localState.email,
+          phone: localState.phone,
+          notes: localState.notes,
         },
       });
 
@@ -40,13 +52,67 @@ const LocalAuthoritySignUp: FC<LocalAuthoritySignUpProps> = ({ name, setStage })
     <div className={styles.card}>
       <h1>{name}</h1>
       <hr />
-      <TextInput header="First name" />
-      <TextInput header="Last name" />
-      <TextInput header="Job title or role" />
-      <TextInput header="Department" />
-      <TextInput header="Email" />
-      <TextInput header="Phone" />
+      <TextInput
+        header="First name"
+        onChange={(event) => {
+          setLocalState((prevState) => ({
+            ...prevState,
+            firstName: event.target.value,
+          }));
+        }}
+      />
+      <TextInput
+        header="Last name"
+        onChange={(event) => {
+          setLocalState((prevState) => ({
+            ...prevState,
+            lastName: event.target.value,
+          }));
+        }}
+      />
+      <TextInput
+        header="Job title or role"
+        onChange={(event) => {
+          setLocalState((prevState) => ({
+            ...prevState,
+            jobTitle: event.target.value,
+          }));
+        }}
+      />
+      <TextInput
+        header="Department"
+        onChange={(event) => {
+          setLocalState((prevState) => ({
+            ...prevState,
+            department: event.target.value,
+          }));
+        }}
+      />
+      <TextInput
+        header="Email"
+        onChange={(event) => {
+          setLocalState((prevState) => ({
+            ...prevState,
+            email: event.target.value,
+          }));
+        }}
+      />
+      <TextInput
+        header="Phone"
+        onChange={(event) => {
+          setLocalState((prevState) => ({
+            ...prevState,
+            phone: event.target.value,
+          }));
+        }}
+      />
       <TextArea
+        onChange={(event) => {
+          setLocalState((prevState) => ({
+            ...prevState,
+            notes: event.target.value,
+          }));
+        }}
         header="Notes about this user (optional)"
         subHeading="This information can only be seen by Donate to Educate administrators."
         characterLimit={1000}
