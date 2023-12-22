ea0543dc0320bbb2cda228be7c66716f83fba059
Robin Moffatt <robinmoffatt@Robins-MacBook-Pro.local>
2023-12-22 14:34:34 +0000

    feat: add cookie banner

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -12,6 +12,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import PrivateRoute from '../PrivateRoute/PrivateRoute';
 import { ConfigProvider } from 'antd';
+import CookieBanner from '../CookieBanner/CookieBanner';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -25,40 +26,43 @@ const App = (): JSX.Element => {
   const isMobile = useMediaQuery({ query: `(max-width: ${breakpoints.screenMedium})` });
 
   return (
-    <ConfigProvider theme={{ hashed: false, token: { fontFamily: 'Poppins' } }}>
-      <QueryClientProvider client={queryClient}>
-        <Suspense>
-          <ErrorBoundary name="Generic">
-            <Router>
-              <Routes>
-                {routes.map(({ element, path, requiresAuth, redirectRoute }, index) => (
-                  <Route
-                    key={index}
-                    element={
-                      <Layout
-                        page={
-                          <ErrorBoundary name="Router">
-                            {requiresAuth ? (
-                              <PrivateRoute route={redirectRoute}>{element}</PrivateRoute>
-                            ) : (
-                              element
-                            )}
-                          </ErrorBoundary>
-                        }
-                        header={isMobile ? <Sidebar /> : <Navbar />}
-                        footer={<Footer />}
-                      />
-                    }
-                    path={path}
-                  />
-                ))}
-              </Routes>
-            </Router>
-          </ErrorBoundary>
-        </Suspense>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </ConfigProvider>
+    <>
+      <ConfigProvider theme={{ hashed: false, token: { fontFamily: 'Poppins' } }}>
+        <QueryClientProvider client={queryClient}>
+          <Suspense>
+            <ErrorBoundary name="Generic">
+              <Router>
+                <Routes>
+                  {routes.map(({ element, path, requiresAuth, redirectRoute }, index) => (
+                    <Route
+                      key={index}
+                      element={
+                        <Layout
+                          page={
+                            <ErrorBoundary name="Router">
+                              {requiresAuth ? (
+                                <PrivateRoute route={redirectRoute}>{element}</PrivateRoute>
+                              ) : (
+                                element
+                              )}
+                            </ErrorBoundary>
+                          }
+                          header={isMobile ? <Sidebar /> : <Navbar />}
+                          footer={<Footer />}
+                        />
+                      }
+                      path={path}
+                    />
+                  ))}
+                </Routes>
+              </Router>
+            </ErrorBoundary>
+          </Suspense>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </ConfigProvider>
+      <CookieBanner />
+    </>
   );
 };
 
