f637e049453703bebcfcae603fff721d75cf9cc6
Ryan Smith <ryan.smith1@bp.com>
2024-01-20 20:12:36 +0000

    feat: replace homepage with new designs

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,14 +2,7 @@ import { FC } from 'react';
 import styles from './Home.module.scss';
 import Image from '@components/Image/Image';
 import westSussexCouncilLogo from '@assets/logo/WestSussexCouncilLogo.webp';
-import Header from '@components/Header/Header';
-import InfoTile from '@/components/InfoTile/InfoTile';
-import SustainabilityAndCircularity from './SustainabilityAndCircularity/SustainabilityAndCircularity';
 import { HeroBanner } from '@/components/HeroBanner/HeroBanner';
-import Carousel from '@/components/Carousel/Carousel';
-import Laptop from '@/assets/carousel/Laptop.webp';
-import Tablet from '@/assets/carousel/Tablet.webp';
-import Tiles from '@/assets/carousel/Tiles.webp';
 import Button from '@/components/Button/Button';
 import { useNavigate } from 'react-router-dom';
 import { Pill } from '@/components/Pill/Pill';
@@ -132,41 +125,15 @@ const Home: FC = () => {
         </div>
       </div>
       <HeroBanner />
-      <Carousel
-        items={[
-          {
-            title:
-              'A platform to unite communities to collect items and match need to availability',
-            image: Laptop,
-            colour: 'lightBlue',
-          },
-          {
-            title:
-              'Parents and guardians can request available items from their school or local community groups.',
-            image: Tablet,
-            colour: 'midBlue',
-          },
-          {
-            title:
-              'Schools can connect to local community groups to list the items they need, items available and excess stock they can share',
-            image: Tiles,
-            colour: 'darkBlue',
-          },
-        ]}
-      />
-      <InfoTile colour="lightBlue" />
-      <InfoTile colour="midBlue" />
-      <InfoTile colour="darkBlue" />
-      <SustainabilityAndCircularity />
+
       <div className={styles.councilBanner}>
-        <Header
-          className={styles.title}
-          text="First launching in partnership with West Sussex County&nbsp;Council"
-        />
+        <h2 className={styles.title}>
+          First launching in partnership with West Sussex County Council
+        </h2>
         <Image
           image={westSussexCouncilLogo}
           alt="west sussex county council logo"
-          width={300}
+          width={250}
           className={styles.westSussexLogo}
         />
       </div>
