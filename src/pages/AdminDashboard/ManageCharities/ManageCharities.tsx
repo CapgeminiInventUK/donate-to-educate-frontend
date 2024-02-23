3b60207bcca387088b10c395395b5cbb288ca838
Ryan Smith <ryan.smith1@bp.com>
2024-02-23 13:47:24 +0000

    feat: clean up charity page

--- src/pages/AdminDashboard/ManageCharities/ManageCharities.tsx
+++ src/pages/AdminDashboard/ManageCharities/ManageCharities.tsx
@@ -1,200 +1,148 @@
-import { useState, useRef, FC } from 'react';
-import Highlighter from 'react-highlight-words';
+import { FC } from 'react';
+// import Highlighter from 'react-highlight-words';
 import { useNavigate } from 'react-router-dom';
-import { Button as SearchButton, Input, InputRef, Space, Table } from 'antd';
-import { FilterConfirmProps } from 'antd/es/table/interface';
-import { ColumnType } from 'antd/es/table';
-import { SearchOutlined, FilterFilled } from '@ant-design/icons';
-import { GraphQLQuery } from 'aws-amplify/api';
+import { Table } from 'antd';
+// import { FilterConfirmProps } from 'antd/es/table/interface';
+// import { ColumnType } from 'antd/es/table';
+// import { SearchOutlined, FilterFilled } from '@ant-design/icons';
+// import { GraphQLQuery } from 'aws-amplify/api';
 import { signOut } from 'aws-amplify/auth';
-import { getAdminPageRequests } from '@/graphql/composite';
-import { client } from '@/graphqlClient';
-import { useQuery } from '@tanstack/react-query';
-import { Pill } from '@/components/Pill/Pill';
+// import { getAdminPageRequests } from '@/graphql/composite';
+// import { client } from '@/graphqlClient';
+// import { useQuery } from '@tanstack/react-query';
+// import { Pill } from '@/components/Pill/Pill';
 import Button from '@/components/Button/Button';
 import BackButton from '@/components/BackButton/BackButton';
 import Spinner from '@/components/Spinner/Spinner';
-import { GetJoinRequestsQuery, GetLocalAuthoritiesQuery, LocalAuthority } from '@/types/api';
+// import { GetJoinRequestsQuery, GetLocalAuthoritiesQuery, LocalAuthority } from '@/types/api';
 import Paths from '@/config/paths';
 import dashboardStyles from '../AdminDashboard.module.scss';
-import styles from './ManageLocalAuthorities.module.scss';
+import styles from './ManageCharities.module.scss';
 
 const ManageCharities: FC = () => {
-  const [searchText, setSearchText] = useState('');
-  const [searchedColumn, setSearchedColumn] = useState('');
-  const searchInput = useRef<InputRef>(null);
+  // const [searchText, setSearchText] = useState('');
+  // const [searchedColumn, setSearchedColumn] = useState('');
+  // const searchInput = useRef<InputRef>(null);
 
   const navigate = useNavigate();
 
-  const { data, isLoading } = useQuery({
-    queryKey: ['la'],
-    queryFn: async () => {
-      const { data } = await client.graphql<
-        GraphQLQuery<GetLocalAuthoritiesQuery & GetJoinRequestsQuery>
-      >({
-        query: getAdminPageRequests,
-      });
+  // const { data, isLoading } = useQuery({
+  //   queryKey: ['charities'],
+  //   queryFn: async () => {
+  //     const { data } = await client.graphql<
+  //       GraphQLQuery<GetLocalAuthoritiesQuery & GetJoinRequestsQuery>
+  //     >({
+  //       query: getAdminPageRequests,
+  //     });
 
-      return data;
-    },
-  });
+  //     return data;
+  //   },
+  // });
 
-  const { registered, notRegistered } =
-    data?.getLocalAuthorities.reduce(
-      (acc, la) => {
-        if (la.registered) {
-          acc.registered++;
-        } else {
-          acc.notRegistered++;
-        }
-        return acc;
-      },
-      {
-        registered: 0,
-        notRegistered: 0,
-      }
-    ) ?? {};
+  // const handleSearch = (
+  //   selectedKeys: string[],
+  //   confirm: (param?: FilterConfirmProps) => void,
+  //   dataIndex: string
+  // ): void => {
+  //   confirm();
+  //   setSearchText(selectedKeys[0]);
+  //   setSearchedColumn(dataIndex);
+  // };
 
-  const handleSearch = (
-    selectedKeys: string[],
-    confirm: (param?: FilterConfirmProps) => void,
-    dataIndex: string
-  ): void => {
-    confirm();
-    setSearchText(selectedKeys[0]);
-    setSearchedColumn(dataIndex);
-  };
+  // const handleReset = (clearFilters: () => void): void => {
+  //   clearFilters();
+  //   setSearchText('');
+  // };
 
-  const handleReset = (clearFilters: () => void): void => {
-    clearFilters();
-    setSearchText('');
-  };
-
-  const getColumnSearchProps = (dataIndex: keyof LocalAuthority): ColumnType<LocalAuthority> => ({
-    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
-      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
-        <Input
-          ref={searchInput}
-          placeholder={`Search ${dataIndex}`}
-          value={selectedKeys[0]}
-          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-          onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
-          style={{ marginBottom: 8, display: 'block' }}
-        />
-        <Space>
-          <SearchButton
-            type="primary"
-            onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
-            icon={<SearchOutlined />}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Search
-          </SearchButton>
-          <SearchButton
-            onClick={() => clearFilters && handleReset(clearFilters)}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Reset
-          </SearchButton>
-          <SearchButton
-            type="link"
-            size="small"
-            onClick={() => {
-              close();
-            }}
-          >
-            Close
-          </SearchButton>
-        </Space>
-      </div>
-    ),
-    filterIcon: () => <SearchOutlined className={styles.filterIcon} />,
-    onFilter: (value, record): boolean => {
-      return record[dataIndex]
-        .toString()
-        .toLowerCase()
-        .includes((value as string).toLowerCase());
-    },
-    onFilterDropdownOpenChange: (visible): void => {
-      if (visible) {
-        setTimeout(() => searchInput.current?.select(), 100);
-      }
-    },
-    render: (text: string) =>
-      searchedColumn === dataIndex ? (
-        <Highlighter
-          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
-          searchWords={[searchText]}
-          autoEscape
-          textToHighlight={text ? text.toString() : ''}
-        />
-      ) : (
-        text
-      ),
-  });
+  // const getColumnSearchProps = (dataIndex: keyof LocalAuthority): ColumnType<LocalAuthority> => ({
+  //   filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+  //     <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+  //       <Input
+  //         ref={searchInput}
+  //         placeholder={`Search ${dataIndex}`}
+  //         value={selectedKeys[0]}
+  //         onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+  //         onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+  //         style={{ marginBottom: 8, display: 'block' }}
+  //       />
+  //       <Space>
+  //         <SearchButton
+  //           type="primary"
+  //           onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+  //           icon={<SearchOutlined />}
+  //           size="small"
+  //           style={{ width: 90 }}
+  //         >
+  //           Search
+  //         </SearchButton>
+  //         <SearchButton
+  //           onClick={() => clearFilters && handleReset(clearFilters)}
+  //           size="small"
+  //           style={{ width: 90 }}
+  //         >
+  //           Reset
+  //         </SearchButton>
+  //         <SearchButton
+  //           type="link"
+  //           size="small"
+  //           onClick={() => {
+  //             close();
+  //           }}
+  //         >
+  //           Close
+  //         </SearchButton>
+  //       </Space>
+  //     </div>
+  //   ),
+  //   filterIcon: () => <SearchOutlined className={styles.filterIcon} />,
+  //   onFilter: (value, record): boolean => {
+  //     return record[dataIndex]
+  //       .toString()
+  //       .toLowerCase()
+  //       .includes((value as string).toLowerCase());
+  //   },
+  //   onFilterDropdownOpenChange: (visible): void => {
+  //     if (visible) {
+  //       setTimeout(() => searchInput.current?.select(), 100);
+  //     }
+  //   },
+  //   render: (text: string) =>
+  //     searchedColumn === dataIndex ? (
+  //       <Highlighter
+  //         highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+  //         searchWords={[searchText]}
+  //         autoEscape
+  //         textToHighlight={text ? text.toString() : ''}
+  //       />
+  //     ) : (
+  //       text
+  //     ),
+  // });
 
   const columns = [
     {
-      title: 'Local authority',
+      title: 'Charity',
       dataIndex: 'name',
       key: 'name',
-      ...getColumnSearchProps('name'),
+      // ...getColumnSearchProps('name'),
     },
     {
-      title: 'Status',
-      dataIndex: 'registered',
-      filters: [
-        {
-          text: 'Joined',
-          value: true,
-        },
-        {
-          text: 'Not Joined',
-          value: false,
-        },
-      ],
-      onFilter: (value: boolean | React.Key, record: LocalAuthority): boolean =>
-        record.registered === value,
-      filterIcon: () => <FilterFilled className={styles.filterIcon} />,
-      render: (registered: boolean) =>
-        registered ? <Pill color="blue" text="Joined" /> : <Pill color="red" text="Not Joined" />,
+      title: 'Local Authority',
+      dataIndex: 'localAuthority',
+      // ...getColumnSearchProps('localAuthority'),
     },
     {
       title: 'Action',
-      render: (_: unknown, la: LocalAuthority) =>
-        la.registered ? (
-          <div className={styles.actionsContainer}>
-            <Button
-              theme="link-blue"
-              className={styles.actionButtons}
-              text="View profile"
-              onClick={(): void => {
-                navigate(`${Paths.ADMIN_DASHBOARD_LA_VIEW}?la=${la.name}`);
-              }}
-            />
-            <Button
-              theme="link-blue"
-              className={styles.actionButtons}
-              text="Edit users"
-              onClick={(): void => {
-                navigate(Paths.ADMIN_DASHBOARD);
-              }}
-            />
-          </div>
-        ) : (
-          <div className={styles.actionsContainer}>
-            <Button
-              theme="link-blue"
-              className={styles.actionButtons}
-              text="Add user"
-              onClick={(): void => {
-                navigate(`${Paths.ADMIN_DASHBOARD_SIGN_UP}?la=${la.name}`);
-              }}
-            />
-          </div>
-        ),
+      render: () => (
+        <div className={styles.actionsContainer}>
+          <Button
+            theme="link-blue"
+            className={styles.actionButtons}
+            text="Remove"
+            onClick={(): void => undefined}
+          />
+        </div>
+      ),
     },
   ];
 
@@ -217,20 +165,15 @@ const ManageCharities: FC = () => {
         </div>
         <div className={dashboardStyles.body}>
           <BackButton theme="white" />
-          {isLoading && <Spinner />}
-          {!isLoading && (
+          {false && <Spinner />}
+          {true && (
             <div className={styles.cardContainer}>
-              <div className={styles.lasCard}>
-                <div className={styles.laBorder}>{registered} joined</div>
-                <div className={styles.laBorder}>{notRegistered} to join</div>
+              <h1>Registered charities and volunteer groups</h1>
+              <div className={styles.charitiesCard}>
+                <div className={styles.charitiesBorder}>0 joined</div>
                 <br />
 
-                <Table
-                  className={styles.lasTable}
-                  dataSource={data?.getLocalAuthorities}
-                  columns={columns}
-                  scroll={{ x: 'max-content' }}
-                />
+                <Table dataSource={[]} columns={columns} scroll={{ x: 'max-content' }} />
               </div>
             </div>
           )}
