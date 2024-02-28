b72d1a1add2e50063bf20e367d227575dc17d90b
Ryan Smith <ryan.smith1@bp.com>
2024-02-28 12:07:54 +0000

    feat: css and html updates to match the design

--- src/pages/RequestSchoolProducts/RequestSchoolProducts.tsx
+++ src/pages/RequestSchoolProducts/RequestSchoolProducts.tsx
@@ -4,6 +4,7 @@ import RadioGroup from '@/components/RadioGroup/RadioGroup';
 import TextInput from '@/components/TextInput/TextInput';
 import TextArea from '@/components/TextArea/TextArea';
 import FormButton from '@/components/FormButton/FormButton';
+import BackButton from '@/components/BackButton/BackButton';
 
 const RequestSchoolProducts: FC = () => {
   const [name, setName] = useState<string>();
@@ -32,30 +33,35 @@ const RequestSchoolProducts: FC = () => {
   };
   return (
     <div className={styles.container}>
-      <p className={styles.mainHeading}>Request school products</p>
-      <p>
-        Tell us which things you need and we&apos;ll contact you to arrange the next steps as soon
-        as we can.
-      </p>
+      <div className={styles.contentContainer}>
+        <BackButton theme="blue" />
+        <div className={styles.card}>
+          <p className={styles.mainHeading}>Request school products</p>
+          <p>
+            Tell us which things you need and we&apos;ll contact you to arrange the next steps as
+            soon as we can.
+          </p>
 
-      <h3 className={styles.subHeading}>What best describes you?</h3>
-      <RadioGroup
-        labels={radioButtonLabels}
-        name="schoolProductRadios"
-        values={radioButtonValues}
-      />
-      <h3 className={styles.fieldHeadings}>Name</h3>
-      <TextInput onChange={onNameChange} value={name} isLarge={true} />
-      <h3 className={styles.fieldHeadings}>Email</h3>
-      <TextInput onChange={onEmailChange} value={email} isLarge={true} />
-      <h3 className={styles.fieldHeadings}>Phone</h3>
-      <TextInput onChange={onPhoneChange} value={phone} />
-      <h3 className={styles.textAreaHeadings}>Tell us what you need</h3>
-      <TextArea
-        characterLimit={1000}
-        subHeading="Include the school products and sizes you would like"
-      />
-      <FormButton text={'Request products'} theme={'formButtonGreen'} fullWidth={true} />
+          <h3 className={styles.subHeading}>What best describes you?</h3>
+          <RadioGroup
+            labels={radioButtonLabels}
+            name="schoolProductRadios"
+            values={radioButtonValues}
+          />
+          <h3 className={styles.fieldHeadings}>Name</h3>
+          <TextInput onChange={onNameChange} value={name} isLarge={true} />
+          <h3 className={styles.fieldHeadings}>Email</h3>
+          <TextInput onChange={onEmailChange} value={email} isLarge={true} />
+          <h3 className={styles.fieldHeadings}>Phone</h3>
+          <TextInput onChange={onPhoneChange} value={phone} />
+          <h3 className={styles.textAreaHeadings}>Tell us what you need</h3>
+          <TextArea
+            characterLimit={1000}
+            subHeading="Include the school products and sizes you would like"
+          />
+          <FormButton text={'Request products'} theme={'formButtonGreen'} fullWidth={true} />
+        </div>
+      </div>
     </div>
   );
 };
