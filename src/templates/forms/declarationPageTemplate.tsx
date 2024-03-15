f9ecc8053dc6ca05c1cb3acaca8488bbc4b4a61b
Jake Readman <jakereadman@gmail.com>
2024-03-15 12:37:04 +0000

    add declaration page template

---
+++ src/templates/forms/declarationPageTemplate.tsx
@@ -0,0 +1,37 @@
+import LogoBlue from '@/assets/logo/LogoBlue';
+import Paths from '@/config/paths';
+import { ComponentType, FormTemplate } from '@/types/data';
+import { Link } from 'react-router-dom';
+
+const getDeclarationPageTemplate = (): FormTemplate => {
+  return {
+    logo: <LogoBlue />,
+    header: 'Read our privacy policy',
+    subHeader: (
+      <>
+        Before you join Donate to Education, you must read our{' '}
+        <Link target="__blank" to={Paths.PRIVACY_POLICY}>
+          privacy policy (opens in new tab
+        </Link>
+        )
+      </>
+    ),
+    isDeclarationPage: true,
+    formComponents: [
+      {
+        componentType: ComponentType.CHECKBOX,
+        componentData: {
+          ariaLabel: 'privacy policy',
+          label: 'I have read the Donate to Educate privacy policy',
+          formMeta: {
+            page: 4,
+            field: 'I have read the Donate to Educate privacy policy',
+          },
+        },
+        classNameSuffix: 'checkbox',
+      },
+    ],
+  };
+};
+
+export default getDeclarationPageTemplate;
