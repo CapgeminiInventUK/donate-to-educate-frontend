b84bec5f95264cf9b85cf9d639926d187a4115b8
Ryan Smith <ryan.smith1@bp.com>
2024-03-15 11:07:18 +0000

    feat: prevent navigation to page without state

--- src/pages/SchoolsDashboard/SchoolsDashboard.tsx
+++ src/pages/SchoolsDashboard/SchoolsDashboard.tsx
@@ -3,18 +3,19 @@ import PublicDashboard from '@/components/PublicDashboard/PublicDashboard';
 import { useQuery } from '@tanstack/react-query';
 import { client } from '@/graphqlClient';
 import { GraphQLQuery } from 'aws-amplify/api';
-import { useLocation } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { GetSchoolProfileQuery } from '@/types/api';
 import { getSchoolProfile } from '@/graphql/queries';
 import Spinner from '@/components/Spinner/Spinner';
+import Paths from '@/config/paths';
 
 const SchoolsDashboard: FC = () => {
   const location = useLocation();
-  const { name, urn } = location?.state as { name: string; urn: string };
-  // eslint-disable-next-line no-console
-  console.log(name, urn);
+  const { name, urn } = (location?.state as { name: string; urn: string }) || {};
+
   const { isLoading, data } = useQuery({
     queryKey: [`school-profile-${name}-${urn}`],
+    enabled: location?.state !== undefined,
     queryFn: async () => {
       const { data } = await client.graphql<GraphQLQuery<GetSchoolProfileQuery>>({
         query: getSchoolProfile,
@@ -27,6 +28,10 @@ const SchoolsDashboard: FC = () => {
       return data;
     },
   });
+
+  if (!name || !urn) {
+    return <Navigate to={Paths.FIND_YOUR_COMMUNITY} />;
+  }
 
   if (isLoading) {
     return <Spinner />;
