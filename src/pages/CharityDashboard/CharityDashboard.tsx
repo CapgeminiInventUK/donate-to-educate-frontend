abe14cd0e77fc69708b5896e5e2a7bf7416d86ed
Ryan Smith <ryan.smith1@bp.com>
2024-03-15 10:49:04 +0000

    feat: correct build issue

--- src/pages/CharityDashboard/CharityDashboard.tsx
+++ src/pages/CharityDashboard/CharityDashboard.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 import PublicDashboard from '@/components/PublicDashboard/PublicDashboard';
 
 const CharityDashboard: FC = () => {
-  return <PublicDashboard type="charity" />;
+  return <PublicDashboard type="charity" name="" />;
 };
 
 export default CharityDashboard;
