bf7b48e8e39d2388c0f30066c8425a63f1d808ee
Ryan Smith <ryan.smith1@bp.com>
2024-03-22 16:04:02 +0000

    feat: plugin public screens with actual data

--- src/pages/Charity/Public/CharityEdit.tsx
+++ src/pages/Charity/Public/CharityEdit.tsx
@@ -1,9 +1,25 @@
 import { FC } from 'react';
 
 import ItemSelection from '@/components/ItemSelection/ItemSelection';
+import { useLocation } from 'react-router-dom';
+import { ProfileItems } from '@/types/api';
 
 const CharityEdit: FC = () => {
-  return <ItemSelection schoolOrCharity="charity" />;
+  const location = useLocation();
+
+  const { profile } = (location?.state as { profile: ProfileItems }) ?? {};
+  const { items, actionText, whatToExpect } = profile ?? {};
+
+  const parsedItems = JSON.parse(items ?? '{}') as Record<number, string[]>;
+
+  return (
+    <ItemSelection
+      schoolOrCharity="charity"
+      items={parsedItems}
+      whatToExpect={whatToExpect ?? ''}
+      actionText={actionText ?? ''}
+    />
+  );
 };
 
 export default CharityEdit;
--- src/pages/School/Public/SchoolEdit.tsx
+++ src/pages/School/Public/SchoolEdit.tsx
@@ -1,9 +1,23 @@
 import { FC } from 'react';
-
 import ItemSelection from '@/components/ItemSelection/ItemSelection';
+import { useLocation } from 'react-router-dom';
+import { ProfileItems } from '@/types/api';
 
 const SchoolEdit: FC = () => {
-  return <ItemSelection schoolOrCharity="school" />;
+  const location = useLocation();
+
+  const { profile } = (location?.state as { profile: ProfileItems }) ?? {};
+  const { items, actionText, whatToExpect } = profile ?? {};
+
+  const parsedItems = JSON.parse(items ?? '{}') as Record<number, string[]>;
+  return (
+    <ItemSelection
+      schoolOrCharity="school"
+      items={parsedItems}
+      actionText={actionText ?? ''}
+      whatToExpect={whatToExpect ?? ''}
+    />
+  );
 };
 
 export default SchoolEdit;
