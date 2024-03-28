54efd9cd04d373c08878b97f90556c0c7dbd08f0
Ryan Smith <ryan.smith1@bp.com>
2024-03-28 10:09:38 +0000

    feat: refresh privacy policy page

--- src/components/FooterPage/FooterPage.tsx
+++ src/components/FooterPage/FooterPage.tsx
@@ -3,11 +3,13 @@ import styles from './FooterPage.module.scss';
 import Paths from '@/config/paths';
 import { Link } from 'react-router-dom';
 import { FooterPageProps } from '@/types/props';
+import BackButton from '../BackButton/BackButton';
 
 const FooterPage: FC<FooterPageProps> = ({ title, children }) => {
   return (
     <div className={styles.container}>
       <div className={styles.contentContainer}>
+        <BackButton theme={'blue'} />
         <h1 className={styles.title}>{title}</h1>
         <div className={styles.card}>
           {children}
