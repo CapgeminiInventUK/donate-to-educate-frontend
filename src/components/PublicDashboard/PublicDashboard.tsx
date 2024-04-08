fe533c48ff5f0582d5c912baef10e2ea053b3b60
Ryan Smith <ryan.smith1@bp.com>
2024-04-08 09:01:57 +0100

    fix: correct banner to make less buggy

--- src/components/PublicDashboard/PublicDashboard.tsx
+++ src/components/PublicDashboard/PublicDashboard.tsx
@@ -34,7 +34,8 @@ const PublicDashboard: FC<PublicDashboardProps> = ({
   setPreview,
 }) => {
   const navigate = useNavigate();
-
+  // eslint-disable-next-line no-console
+  console.log(header);
   return (
     <>
       <InstitutionBanner
