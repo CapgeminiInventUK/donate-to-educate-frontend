96f04f1b68b30408176b38b195528dfc88623d36
Ryan Smith <ryan.smith1@bp.com>
2024-04-16 14:53:14 +0100

    feat: link contact us

--- src/components/CannotFindSchool/CannotFindSchool.tsx
+++ src/components/CannotFindSchool/CannotFindSchool.tsx
@@ -1,11 +1,15 @@
 import { FC } from 'react';
 import styles from './CannotFindSchool.module.scss';
+import { Link } from 'react-router-dom';
+import Paths from '@/config/paths';
 
 const CannotFindSchool: FC = () => {
   return (
     <div className={styles.container}>
       <h2>I cannot find my school</h2>
-      <p>If you cannot find your school, contact us for help.</p>
+      <p>
+        If you cannot find your school, <Link to={Paths.CONTACT}>contact us for help.</Link>
+      </p>
     </div>
   );
 };
