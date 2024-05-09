7955f3d9709273f0442dd8ca0320608018b484cd
jolayton <jonathan.layton@capgemini.com>
2024-05-09 10:54:50 +0100

    Update to logoselector

--- src/pages/LocalAuthorityDashboard/LogoSelector/LogoSelector.tsx
+++ src/pages/LocalAuthorityDashboard/LogoSelector/LogoSelector.tsx
@@ -1,10 +1,9 @@
 import { FC } from 'react';
 import logoComponents from './logoComponents';
-import westSussex from '@assets/countyLogos/westSussex.svg';
 
 const LogoSelector: FC<{ name: string }> = ({ name }) => {
   const LogoComponent = logoComponents[name];
-  return LogoComponent ? <img src={westSussex} alt={name} /> : null;
+  return LogoComponent ? <img src={LogoComponent} alt={name} /> : null;
 };
 
 export default LogoSelector;
