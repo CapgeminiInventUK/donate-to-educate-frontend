6413c529046c091b27382fcb2a6a4299cf1483df
jolayton <jonathan.layton@capgemini.com>
2024-05-15 11:15:45 +0100

    Updated footerpage component

--- src/components/FooterPage/FooterPage.tsx
+++ src/components/FooterPage/FooterPage.tsx
@@ -1,14 +1,23 @@
 import { FC } from 'react';
 import styles from './FooterPage.module.scss';
+import Paths from '@/config/paths';
+import { Link } from 'react-router-dom';
 import { FooterPageProps } from '@/types/props';
 import Card from '@/components/Card/Card';
 
-const FooterPage: FC<FooterPageProps> = ({ title, children }) => {
+const FooterPage: FC<FooterPageProps> = ({ title, homepageLink = true, children }) => {
   return (
     <div className={styles.container}>
       <div className={styles.contentContainer}>
         <h1 className={styles.title}>{title}</h1>
-        <Card className={styles.footerCard}>{children}</Card>
+        <Card className={styles.footerCard}>
+          {children}
+          {homepageLink && (
+            <Link className={styles.home} to={Paths.HOME}>
+              Return to homepage
+            </Link>
+          )}
+        </Card>
       </div>
     </div>
   );
