8c0c04d3901b1c6aab0040a5fa496b6a51545072
Jake Readman <jakereadman@gmail.com>
2024-05-17 18:38:59 +0100

    add tick icon

---
+++ src/assets/sign-up/tick.tsx
@@ -0,0 +1,15 @@
+const TickIcon = (): JSX.Element => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+      <path
+        d="M22 5L7.08434 20L2 14.8869"
+        stroke="#11356F"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default TickIcon;
