ed7eecdabedc90e139b39975f185bf67f64eed44
Jake Readman <jakereadman@gmail.com>
2024-06-03 10:19:02 +0100

    update close icon

--- src/components/CloseButton/CloseButton.tsx
+++ src/components/CloseButton/CloseButton.tsx
@@ -1,27 +1,12 @@
 import Close from '@/assets/tiles/Close';
 import { CloseButtonProps } from '@/types/props';
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import styles from './CloseButton.module.scss';
 
 const CloseButton: FC<CloseButtonProps> = ({ onClick }) => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const handleMouseEnter = (): void => {
-    setIsHovered(true);
-  };
-
-  const handleMouseLeave = (): void => {
-    setIsHovered(false);
-  };
-
   return (
-    <button
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      className={styles.closeButton}
-      onClick={onClick}
-    >
-      <Close className={styles.closeIcon} hover={isHovered} />
+    <button className={styles.closeButton} onClick={onClick}>
+      <Close />
     </button>
   );
 };
