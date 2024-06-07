da6e2a71965533e1c66ba41f6d96139a8da7030b
Jake Readman <jakereadman@gmail.com>
2024-06-07 10:33:21 +0100

    test coverage

---
+++ src/components/AddressInset/AddressInset.test.tsx
@@ -0,0 +1,42 @@
+import { render } from '@testing-library/react';
+import AddressInset from './AddressInset';
+import { FormSections } from '@/types/data';
+
+describe('Address Inset', () => {
+  test('should not render if no name, localAuthority and postcode', () => {
+    const componentData = {
+      header: 'Join Donate to Educate',
+      infoText: 'To add your school, you need to:',
+    };
+    const { queryByTestId } = render(<AddressInset formData={[]} componentData={componentData} />);
+    expect(queryByTestId('address-inset')).toBeNull();
+  });
+
+  test('should render if name, local authority and postcode exist in form data', () => {
+    const formData = [
+      {
+        field: 'School',
+        value: 'Newport School - E10 6PJ',
+        fullValue: {
+          value: '103048',
+          label: 'Newport School - E10 6PJ',
+          name: 'Newport School',
+          localAuthority: 'Waltham Forest',
+          registrationState: 'laNotRegistered',
+          postcode: 'E10 6PJ',
+          registered: false,
+        },
+      },
+    ];
+    const componentData = {
+      subHeading: "Enter your school's name or postcode.",
+      formMeta: { page: 1, field: 'School', section: FormSections.YOUR_DETAILS_SECTION },
+      options: [],
+      ariaLabel: 'label',
+    };
+    const { queryByTestId } = render(
+      <AddressInset formData={formData} componentData={componentData} />
+    );
+    expect(queryByTestId('address-inset')).not.toBeNull();
+  });
+});
