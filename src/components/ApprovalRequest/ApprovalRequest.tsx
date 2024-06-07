4671dbded5fd253837fb23fbdb26658ebf56ad1d
Jake Readman <jakereadman@gmail.com>
2024-06-07 12:10:14 +0100

    refactor approval request

--- src/components/ApprovalRequest/ApprovalRequest.tsx
+++ src/components/ApprovalRequest/ApprovalRequest.tsx
@@ -1,11 +1,7 @@
 import { FC, useEffect, useState } from 'react';
 import styles from './ApprovalRequest.module.scss';
 import BackButton from '@/components/BackButton/BackButton';
-import Phone from '@/assets/admin/Phone';
-import ToolTip from '@/assets/admin/ToolTip';
-import DeclineDeleteModal from '../DeclineDeleteModal/DeclineDeleteModal';
 import ResultBanner from './ResultBanner/ResultBanner';
-import FormButton from '@/components/FormButton/FormButton';
 import { Pill } from '@/components/Pill/Pill';
 import { useQuery } from '@tanstack/react-query';
 import { client } from '@/graphqlClient';
@@ -16,15 +12,16 @@ import {
   UpdateJoinRequestMutation,
 } from '@/types/api';
 import { deleteDeniedJoinRequest, updateJoinRequest } from '@/graphql/mutations';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Paths from '@/config/paths';
 import { getSchool } from '@/graphql/queries';
 import Spinner from '@/components/Spinner/Spinner';
-import Globe from '@/assets/tiles/Globe';
 import { ApprovalRequestProps } from '@/types/props';
 import { StageState, myStageType } from '@/types/data';
 import ErrorBanner from '@/components/ErrorBanner/ErrorBanner';
 import Card from '@/components/Card/Card';
+import SchoolDetails from './SchoolDetails/SchoolDetails';
+import UserRequestDetails from './UserRequestDetails/UserRequestDetails';
 
 const ApprovalRequest: FC<ApprovalRequestProps> = ({
   setStage,
@@ -119,187 +116,20 @@ const ApprovalRequest: FC<ApprovalRequestProps> = ({
             color={type == 'school' ? 'blue' : 'lightBlue'}
             text={type == 'school' ? 'SCHOOL' : 'CHARITY OR VOLUNTEER GROUP'}
           />
-          {type === 'school' && (
-            <>
-              <h1>{data?.getSchool.name}</h1>
-              {data?.getSchool.phone && (
-                <div className={styles.contactInfo}>
-                  <Phone />
-                  {data?.getSchool.phone && (
-                    <Link
-                      to={`tel: ${data?.getSchool.phone}`}
-                      target="_blank"
-                      className={styles.item}
-                    >
-                      {data.getSchool.phone}
-                    </Link>
-                  )}
-                </div>
-              )}
-              {data?.getSchool.website && (
-                <div className={styles.contactInfo}>
-                  <Globe />
-                  {data?.getSchool.website && (
-                    <Link
-                      to={`https://${data.getSchool.website}`}
-                      target="_blank"
-                      className={styles.item}
-                    >
-                      {data.getSchool.website}
-                    </Link>
-                  )}
-                </div>
-              )}
-              <div className={styles.detailsCard}>
-                <p>{data?.getSchool.street}</p>
-                <p>{data?.getSchool.locality}</p>
-                <p>{data?.getSchool.address3}</p>
-                <p>{data?.getSchool.town}</p>
-                <p>{data?.getSchool.county}</p>
-                <p>{data?.getSchool.postcode}</p>
-                <p>England</p>
-              </div>{' '}
-            </>
-          )}
+          {type === 'school' && data && <SchoolDetails data={data} />}
           {type === 'charity' && <h1>{name}</h1>}
           <hr />
-          {myStage === 'deciding' && type === 'school' && (
-            <>
-              <p>
-                To confirm this connection, check that they&apos;re senior staff or a senior contact
-                at the school.
-              </p>
-              <div className={styles.contactInfo}>
-                <ToolTip className={styles.infoToolTip} />
-                <b>The local authority may also review, confirm or decline this request.</b>
-              </div>
-              <div className={styles.requestDecisionCard}>
-                <div className={styles.informationLine}>
-                  <b>Name</b>
-                  <span>{user.name}</span>
-                </div>
-                <hr />
-                <div className={styles.informationLine}>
-                  <b>Job title or role</b>
-                  <span>{user.title}</span>
-                </div>
-                <hr />
-                <div className={styles.informationLine}>
-                  <b>Email</b>
-                  <span>{user.email}</span>
-                </div>
-                <hr />
-                <div className={styles.informationLine}>
-                  <b>Phone</b>
-                  <span>{user.phone}</span>
-                </div>
-                <hr />
-                <div className={styles.actionButtons}>
-                  <FormButton
-                    theme="formButtonMidBlue"
-                    text="Confirm request"
-                    onClick={(): void => {
-                      setMyStage('approved');
-                    }}
-                    ariaLabel="confirm request"
-                  />
-                  <FormButton
-                    theme="formButtonGrey"
-                    text="Decline request"
-                    onClick={(): void => setShowModal(true)}
-                    ariaLabel="decline request"
-                  />
-                </div>
-              </div>
-              <DeclineDeleteModal
-                setShowModal={setShowModal}
-                showModal={showModal}
-                onConfirm={() => setMyStage('denied')}
-                bodyText=" If you cannot identify this connection or confirm they are connected to the school, you
-                may decline them."
-                confirmText="Decline request"
-              />
-            </>
-          )}
-          {myStage === 'deciding' && type === 'charity' && (
-            <>
-              <p>
-                To confirm this connection, check that they&apos;re working at the charity or
-                volunteer group and have somewhere to store products.
-              </p>
-              <div className={styles.contactInfo}>
-                <ToolTip className={styles.infoToolTip} />
-                <b>The local authority may also review, confirm or decline this request.</b>
-              </div>
-              <div className={styles.requestDecisionCard}>
-                <h3>Details</h3>
-                <div className={styles.informationLine}>
-                  <b>Name</b>
-                  <span>{user.name}</span>
-                </div>
-                <hr />
-                <div className={styles.informationLine}>
-                  <b>Job title or role</b>
-                  <span>{user.title}</span>
-                </div>
-                <hr />
-                <div className={styles.informationLine}>
-                  <b>Email</b>
-                  <span>{user.email}</span>
-                </div>
-                <hr />
-                <div className={styles.informationLine}>
-                  <b>Phone</b>
-                  <span>{user.phone}</span>
-                </div>
-                <hr />
-                <h3>Charity or volunteer group</h3>
-                <div className={styles.informationLine}>
-                  <b>Name</b>
-                  <span>{name}</span>
-                </div>
-                <hr />
-                <div className={styles.informationLine}>
-                  <b>Local Authority</b>
-                  <span>{la}</span>
-                </div>
-                <hr />
-                <div className={styles.informationLine}>
-                  <b>Main Address</b>
-                  <span className={styles.address}>{charity?.charityAddress}</span>
-                </div>
-                <hr />
-                <div className={styles.informationLine}>
-                  <b>About</b>
-                  <span>{charity?.aboutCharity}</span>
-                </div>
-                <hr />
-                <div className={styles.actionButtons}>
-                  <FormButton
-                    theme="formButtonMidBlue"
-                    text="Confirm request"
-                    onClick={(): void => {
-                      setMyStage('approved');
-                    }}
-                    ariaLabel="confirm request"
-                  />
-                  <FormButton
-                    theme="formButtonGrey"
-                    text="Decline request"
-                    onClick={(): void => setShowModal(true)}
-                    ariaLabel="decline request"
-                  />
-                </div>
-              </div>
-              <DeclineDeleteModal
-                setShowModal={setShowModal}
-                showModal={showModal}
-                onConfirm={() => setMyStage('denied')}
-                bodyText=" If you cannot identify this connection or confirm they are connected to the school, you
-                may decline them."
-                confirmText="Decline request"
-              />
-            </>
+          {myStage === 'deciding' && (
+            <UserRequestDetails
+              type={type}
+              user={user}
+              showModal={showModal}
+              setShowModal={setShowModal}
+              setMyStage={setMyStage}
+              charity={charity}
+              charityName={name}
+              la={la}
+            />
           )}
         </>
         {myStage === 'approved' && <ResultBanner name={user.name} type="approved" />}
